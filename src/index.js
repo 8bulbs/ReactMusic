f234a17ca6e93fa1a505c62e7e3937f1e80e9dfa
8bulbs <286630571@qq.com>
2018-07-11 21:58:50 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { counter } from './store/index.redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+ReactDOM.render(
+  (<Provider store={store}>
+    <App />
+  </Provider>)
+  , document.getElementById('root'));
 registerServiceWorker();
--- src/store/index.redux.js
+++ src/store/index.redux.js
@@ -1,3 +1,25 @@
 import { createStore } from 'redux'
+// type
+const ADD = '加'
+const DEL = '减'
 
+// 通过reducer建立状态
+export function counter (state = 0, action) {
+  switch (action.type) {
+    case ADD:
+      return state++
+    case DEL:
+      return state--
+    default:
+      return 10
+  }
+}
+
+// action creater
+export function add () {
+  return { type: ADD}
+}
+export function del () {
+  return { type: DEL}
+}
 
